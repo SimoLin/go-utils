54334187da2b4eb0a80e07fc360a18520956b33f
SimoLin <840639486@qq.com>
2025-03-21 18:27:59 +0800

    [+] 优化函数命名

--- common/common.go
+++ common/common.go
@@ -10,6 +10,12 @@ import (
 	"github.com/elastic/elastic-agent-libs/mapstr"
 )
 
+func StringToMap(s string) (M1 map[string]any, err error) {
+	M1 = map[string]any{}
+	err = json.Unmarshal([]byte(s), &M1)
+	return
+}
+
 func MapToString(M1 map[string]any) (r string) {
 	result_byte, err := json.Marshal(M1)
 	if err != nil {
@@ -18,14 +24,17 @@ func MapToString(M1 map[string]any) (r string) {
 	return string(result_byte)
 }
 
-func StringToMap(s string) (M1 map[string]any, err error) {
-	M1 = map[string]any{}
-	err = json.Unmarshal([]byte(s), &M1)
-	return
+// 获取 Map 的 key 值切片
+func MapGetKeys[M ~map[K]V, K comparable, V any](m M) []K {
+	r := make([]K, 0, len(m))
+	for k := range m {
+		r = append(r, k)
+	}
+	return r
 }
 
 // 读取 map 中的值并返回指定类型的值
-func MustGetValue[T any](M1 map[string]any, key string) (result T) {
+func MapGetValue[T any](M1 map[string]any, key string) (result T) {
 	value, err := mapstr.M(M1).GetValue(key)
 	if err != nil {
 		return
@@ -37,7 +46,7 @@ func MustGetValue[T any](M1 map[string]any, key string) (result T) {
 }
 
 // 读取 map 中的值并返回 string 类型的值，其他类型会强制使用 fmt.Sprintf("%v") 转换
-func MustGetValueToString(M1 map[string]any, key string) (result string) {
+func MapGetValueToString(M1 map[string]any, key string) (result string) {
 	value, err := mapstr.M(M1).GetValue(key)
 	if err != nil {
 		return
@@ -49,7 +58,7 @@ func MustGetValueToString(M1 map[string]any, key string) (result string) {
 }
 
 // 将任意类型的数组 S1 转为string字符串，间隔符 sep
-func MustGetStringJoin(S1 []any, sep string) (result string) {
+func SliceToString(S1 []any, sep string) (result string) {
 	switch len(S1) {
 	case 0:
 		return ""
@@ -68,27 +77,8 @@ func MustGetStringJoin(S1 []any, sep string) (result string) {
 	return b.String()
 }
 
-// 正则提取参数值(分组查询)
-func MustGetRegexpSubmatch(str_input string, regex_expression string, index int) (r string) {
-	re_match := regexp.MustCompile(regex_expression)
-	result_match := re_match.FindStringSubmatch(str_input)
-	if len(result_match) > index {
-		r = result_match[index]
-	}
-	return
-}
-
-// 获取 Map 的 key 值切片
-func GetMapKeys[M ~map[K]V, K comparable, V any](m M) []K {
-	r := make([]K, 0, len(m))
-	for k := range m {
-		r = append(r, k)
-	}
-	return r
-}
-
 // 切片去重
-func GetUniqueSlice[T cmp.Ordered](S1 []T) []T {
+func SliceUnique[T cmp.Ordered](S1 []T) []T {
 	size := len(S1)
 	if size == 0 {
 		return []T{}
@@ -102,4 +92,14 @@ func GetUniqueSlice[T cmp.Ordered](S1 []T) []T {
 		}
 	}
 	return newSlices
+}
+
+// 正则提取参数值(分组查询)
+func RegexpSubmatch(str_input string, regex_expression string, index int) (r string) {
+	re_match := regexp.MustCompile(regex_expression)
+	result_match := re_match.FindStringSubmatch(str_input)
+	if len(result_match) > index {
+		r = result_match[index]
+	}
+	return
 }
